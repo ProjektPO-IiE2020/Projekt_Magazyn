32e9bf14902dddd4e2eac851c8c2f4617f3e5b3f
Agnieszka Zając <agnieszkazajac61@gmail.com>
2021-01-24 17:26:18 +0100

    Dokumentacja

--- Magazyn/Magazyn/MagazynEksport.cs
+++ Magazyn/Magazyn/MagazynEksport.cs
@@ -20,6 +20,9 @@ namespace Magazyn
         public List<TowarEksport> ListaEksport { get => _listaEksport; set => _listaEksport = value; }
         public string Nazwa { get => nazwa; set => nazwa = value; }
 
+        /// <summary>
+        /// Niżej znajdują się funkcje niezbędne do obsługi magazynu towarów importowych
+        /// </summary>
         public MagazynEksport()
         {
             nazwa = string.Empty;
@@ -32,13 +35,19 @@ namespace Magazyn
         {
             this.nazwa = nazwa;
         }
-
+        /// <summary>
+        /// Funkcja "UmieśćEksport" pozwala na dodanie do magazynu towaru eksportowego
+        /// </summary>
+        /// <param name="t">dodawany towar</param>
         public void UmiescEksport(TowarEksport t)
         {
             _kolejkaEksport.Enqueue(t);
             iloscTowarow++;
         }
-
+        /// <summary>
+        /// Funkcja "PobierzEksport" pozwala na pobranie aktualnej listy towarów
+        /// </summary>
+        /// <returns>Zwraca listę towarów eksportowych znajdujących sie w magazynie</returns>
         public TowarEksport PobierzEksport()
         {
             if (iloscTowarow == 0)
@@ -48,23 +57,35 @@ namespace Magazyn
             --iloscTowarow;
             return _kolejkaEksport.Dequeue();
         }
-
+        /// <summary>
+        /// Funkcja "WyczyśćEksport" usuwa wszystkie towary eksportowe z magazynu
+        /// </summary>
         public void WyczyscEksport()
         {
             iloscTowarow = 0;
             _kolejkaEksport.Clear();
         }
-
+        /// <summary>
+        /// Funkcja "PodajIloscEksport" pozwala sprawdzić ile znajduje się towarów eksportowych w magazynie
+        /// </summary>
+        /// <returns>Ilość towarów eksportowych w magazynie</returns>
         public int PodajIloscEksport()
         {
             return _kolejkaEksport.Count();
         }
-
+        /// <summary>
+        /// Funkcja "PodajBiezacyEksport" pokazuje który produkt jest najdłużej w magazynie 
+        /// </summary>
+        /// <returns>Pierwszy produkt w kolejce towarów eksportowych</returns>
         public TowarEksport PodajBiezacyEksport()
         {
             return _kolejkaEksport.Peek();
         }
-
+        /// <summary>
+        /// Funkcja "UsunTowarEksport" pozwala na usunięcie danego towaru eksportowego po podaniu jego kodu
+        /// </summary>
+        /// <param name="kod">kod produktu eksportowego, ktory chcemy usunąć</param>
+        /// <returns></returns>
         public bool UsunTowarEksport(string kod)
         {
             Queue<TowarEksport> nowa = new Queue<TowarEksport>();
@@ -79,7 +100,11 @@ namespace Magazyn
             _kolejkaEksport = nowa;
             return f;
         }
-
+        /// <summary>
+        /// Funkcja "ZnajdzTowarEksport" pozwala na wyszukanie produktów o wskazanym typie np. słodycze, mięso etc
+        /// </summary>
+        /// <param name="typ">Typ produktów, które chcemy wyświetlić</param>
+        /// <returns>Lista towarów eksportowych o podanym typie</returns>
         public List<TowarEksport> ZnajdzTowarEksport(Typy typ)
         {
             List<TowarEksport> lista = new List<TowarEksport>();
@@ -96,7 +121,11 @@ namespace Magazyn
             }
             return null;
         }
-
+        /// <summary>
+        /// Funkcja "ZnajdzTowarEksport" pozwala na sprawdzenie i wyszukanie towaru po podaniu jego kodu
+        /// </summary>
+        /// <param name="kod">Kod szukanego towaru</param>
+        /// <returns>Poszukiwany towar lub informacja o braku sprawdzanego towaru w magazynie</returns>
         public TowarEksport ZnajdzTowarEksport(string kod)
         {
             foreach (TowarEksport t in _kolejkaEksport)
@@ -108,13 +137,19 @@ namespace Magazyn
             }
             throw new TowarNotFoundException();
         }
-
+        /// <summary>
+        /// Funkcja sortująca towary eksportowe po cenie (rosnąco)
+        /// </summary>
         public void SortujPoCenieEksport()
         {
             List<TowarEksport> nowa = new List<TowarEksport>(_kolejkaEksport);
             nowa.Sort();
             _kolejkaEksport = new Queue<TowarEksport>(nowa);
         }
+        /// <summary>
+        /// Funkcja sortująca towary importowe po nazwie (alfabetycznie)
+        /// </summary>
+        /// <param name="f"></param>
         public void SortujPoNazwieEksport(bool f)
         {
             List<TowarEksport> nowa = new List<TowarEksport>(_kolejkaEksport);
@@ -123,6 +158,10 @@ namespace Magazyn
                 nowa.Reverse();
             _kolejkaEksport = new Queue<TowarEksport>(nowa);
         }
+        /// <summary>
+        /// Funkcja sortująca towary eksportowe po dacie produkcji (od najwcześniej wypordukowanego)
+        /// </summary>
+        /// <param name="f"></param>
         public void SortujPoDacieProdukcjiEksport(bool f)
         {
             List<TowarEksport> nowa = new List<TowarEksport>(_kolejkaEksport);
@@ -131,6 +170,10 @@ namespace Magazyn
                 nowa.Reverse();
             _kolejkaEksport = new Queue<TowarEksport>(nowa);
         }
+        /// <summary>
+        /// Funkcja sortująca towary eksportowe po dacie ważności w celu wydania produktów z magazynu w oparciu o zasadę FIFO
+        /// </summary>
+        /// <param name="f"></param>
         public void SortujPoDaciePrzydatnosciEksport(bool f)
         {
             List<TowarEksport> nowa = new List<TowarEksport>(_kolejkaEksport);
@@ -139,7 +182,10 @@ namespace Magazyn
                 nowa.Reverse();
             _kolejkaEksport = new Queue<TowarEksport>(nowa);
         }
-
+        /// <summary>
+        /// Funkcja umożliwiająca zapis listy towarów eksportowych do XML
+        /// </summary>
+        /// <param name="nazwaPliku">zapisany plik z listą towarów eksportowych</param>
         public void ZapiszXMLEksport(string nazwaPliku)
         {
             _listaEksport = new List<TowarEksport>(_kolejkaEksport);
@@ -149,7 +195,11 @@ namespace Magazyn
                 xmls.Serialize(sw, _listaEksport);
             }
         }
-
+        /// <summary>
+        /// Funkcja służąca to otwarcia zapisanego pliku XML
+        /// </summary>
+        /// <param name="nazwaPliku">otwierany plik</param>
+        /// <returns>Lista towarów eksportowych</returns>
         public static MagazynEksport OdczytajXMLEksport(string nazwaPliku)
         {
             if (!File.Exists(nazwaPliku))
@@ -165,7 +215,10 @@ namespace Magazyn
             m._kolejkaEksport = new Queue<TowarEksport>(m._listaEksport);
             return m;
         }
-
+        /// <summary>
+        /// Funkjca służąca klonowaniu listy towarów eksportowych
+        /// </summary>
+        /// <returns>Skolnowana lista towarów eksportowych</returns>
         public object CloneEksport()
         {
             MagazynEksport nowyMagazyn = new MagazynEksport();
--- Magazyn/Magazyn/MagazynImport.cs
+++ Magazyn/Magazyn/MagazynImport.cs
@@ -19,7 +19,9 @@ namespace Magazyn
         public Queue<TowarImport> KolejkaImport { get => _kolejkaImport; set => _kolejkaImport = value; }
         public List<TowarImport> ListaImport { get => _listaImport; set => _listaImport = value; }
         public string Nazwa { get => nazwa; set => nazwa = value; }
-
+        /// <summary>
+        /// Niżej znajdują się funkcje niezbędne do obsługi magazynu towarów importowych
+        /// </summary>
         public MagazynImport()
         {
             nazwa = string.Empty;
@@ -27,18 +29,24 @@ namespace Magazyn
             _kolejkaImport = new Queue<TowarImport>();
             _listaImport = new List<TowarImport>();
         }
-
+        
         public MagazynImport(string nazwa) : this()
         {
             this.nazwa = nazwa;
         }
-
+        /// <summary>
+        /// Funkcja "UmieśćImport" pozwala na dodanie do magazynu towaru importowego
+        /// </summary>
+        /// <param name="t">dodawany towar</param>
         public void UmiescImport(TowarImport t)
         {
             _kolejkaImport.Enqueue(t);
             iloscTowarow++;
         }
-
+        /// <summary>
+        /// Funkcja "PobierzImport" pozwala na pobranie aktualnej listy towarów
+        /// </summary>
+        /// <returns>Zwraca listę towarów importowych znajdujących sie w magazynie</returns>
         public TowarImport PobierzImport()
         {
             if (iloscTowarow == 0)
@@ -48,23 +56,35 @@ namespace Magazyn
             --iloscTowarow;
             return _kolejkaImport.Dequeue();
         }
-
+        /// <summary>
+        /// Funkcja "WyczyśćImport" usuwa wszystkie towary importowe z magazynu 
+        /// </summary>
         public void WyczyscImport()
         {
             iloscTowarow = 0;
             _kolejkaImport.Clear();
         }
-
+        /// <summary>
+        /// Funkcja "PodajIloscImport" pozwala sprawdzić ile znajduje się towarów impotowych w magazynie
+        /// </summary>
+        /// <returns>Ilość towarów importowych w magazynie</returns>
         public int PodajIloscImport()
         {
             return _kolejkaImport.Count();
         }
-
+        /// <summary>
+        /// Funkcja "PodajBiezacyImport" pokazuje który produkt jest najdłużej w magazynie 
+        /// </summary>
+        /// <returns>Pierwszy produkt w kolejce towarów importowych</returns>
         public TowarImport PodajBiezacyImport()
         {
             return _kolejkaImport.Peek();
         }
-
+        /// <summary>
+        /// Funkcja "UsunTowarImport" pozwala na usunięcie danego towaru importowego po podaniu jego kodu
+        /// </summary>
+        /// <param name="kod">kod produktu importowego, ktory chcemy usunąć</param>
+        /// <returns></returns>
         public bool UsunTowarImport(string kod)
         {
             Queue<TowarImport> nowa = new Queue<TowarImport>();
@@ -80,7 +100,11 @@ namespace Magazyn
             return f;
         }
 
-
+        /// <summary>
+        /// Funkcja "ZnajdzTowarImport" pozwala na wyszukanie produktów o wskazanym typie np. słodycze, mięso etc
+        /// </summary>
+        /// <param name="typ">Typ produktów, które chcemy wyświetlić</param>
+        /// <returns>Lista towarów importowych o podanym typie</returns>
         public List<TowarImport> ZnajdzTowarImport(Typy typ)
         {
             List<TowarImport> lista = new List<TowarImport>();
@@ -97,7 +121,11 @@ namespace Magazyn
             }
             return null;
         }
-
+        /// <summary>
+        /// Funkcja "ZnajdzTowarImport" pozwala na sprawdzenie i wyszukanie towaru po podaniu jego kodu
+        /// </summary>
+        /// <param name="kod">Kod szukanego towaru</param>
+        /// <returns>Poszukiwany towar lub informacja o braku sprawdzanego towaru w magazynie</returns>
         public TowarImport ZnajdzTowarImport(string kod)
         {
             foreach (TowarImport t in _kolejkaImport)
@@ -109,7 +137,9 @@ namespace Magazyn
             }
             throw new TowarNotFoundException();
         }
-
+        /// <summary>
+        /// Funkcja sortująca towary importowe po cenie (rosnąco)
+        /// </summary>
         public void SortujPoCenieImport()
         {
             List<TowarImport> nowa = new List<TowarImport>(_kolejkaImport);
@@ -117,7 +147,10 @@ namespace Magazyn
             _kolejkaImport = new Queue<TowarImport>(nowa);
         }
 
-
+        /// <summary>
+        /// Funkcja sortująca towary importowe po nazwie (alfabetycznie)
+        /// </summary>
+        /// <param name="f"></param>
         public void SortujPoNazwieImport(bool f)
         {
             List<TowarImport> nowa = new List<TowarImport>(_kolejkaImport);
@@ -126,6 +159,10 @@ namespace Magazyn
                 nowa.Reverse();
             _kolejkaImport = new Queue<TowarImport>(nowa);
         }
+        /// <summary>
+        /// Funkcja sortująca towary importowe po dacie produkcji (od najwcześniej wypordukowanego)
+        /// </summary>
+        /// <param name="f"></param>
         public void SortujPoDacieProdukcjiImport(bool f)
         {
             List<TowarImport> nowa = new List<TowarImport>(_kolejkaImport);
@@ -134,6 +171,10 @@ namespace Magazyn
                 nowa.Reverse();
             _kolejkaImport = new Queue<TowarImport>(nowa);
         }
+        /// <summary>
+        /// Funkcja sortująca towary importowe po dacie ważności w celu wydania produktów z magazynu w oparciu o zasadę FIFO
+        /// </summary>
+        /// <param name="f"></param>
         public void SortujPoDaciePrzydatnosciImport(bool f)
         {
             List<TowarImport> nowa = new List<TowarImport>(_kolejkaImport);
@@ -142,7 +183,10 @@ namespace Magazyn
                 nowa.Reverse();
             _kolejkaImport = new Queue<TowarImport>(nowa);
         }
-
+        /// <summary>
+        /// Funkcja umożliwiająca zapis listy towarów importowych do XML
+        /// </summary>
+        /// <param name="nazwaPliku">zapisany plik z listą towarów importowych</param>
         public void ZapiszXMLImport(string nazwaPliku)
         {
             _listaImport = new List<TowarImport>(_kolejkaImport);
@@ -152,7 +196,11 @@ namespace Magazyn
                 xmls.Serialize(sw, _listaImport);
             }
         }
-
+        /// <summary>
+        /// Funkcja służąca to otwarcia zapisanego pliku XML
+        /// </summary>
+        /// <param name="nazwaPliku">otwierany plik</param>
+        /// <returns>Lista towarów importowych</returns>
         public static MagazynImport OdczytajXMLImport(string nazwaPliku)
         {
             if (!File.Exists(nazwaPliku))
@@ -168,7 +216,10 @@ namespace Magazyn
             m._kolejkaImport = new Queue<TowarImport>(m._listaImport);
             return m;
         }
-
+        /// <summary>
+        /// Funkjca służąca klonowaniu listy towarów importowych
+        /// </summary>
+        /// <returns>Sklonowana lista</returns>
         public object CloneImport()
         {
             MagazynImport nowyMagazyn = new MagazynImport();
